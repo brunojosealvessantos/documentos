b8ca765a372b3550cff7da6e20256dc360f76a77
Bruno Santos <brunosantosengmec@gmail.com>
2020-01-27 19:17:06 -0300

    Criando funções de search e validation

--- js/documentos.js
+++ js/documentos.js
@@ -1,4 +1,9 @@
 function search() {
     var cpf = document.getElementById('inputCPF').value
     console.log(cpf);
+    validation(cpf);
+}
+
+function validation(cpfValue) {
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
 }
