754ae89d9cbdc6074756902e076d50e975eb8f61
Bruno Santos <brunosantosengmec@gmail.com>
2020-01-28 20:48:31 -0300

    Busca da pasta por CPF

--- js/documentos.js
+++ js/documentos.js
@@ -1,18 +1,31 @@
 function search() {
-    var cpf = document.getElementById('inputCPF').value
+    var cpf = document.getElementById('inputCPF').value;
     console.log(cpf);
     validation(cpf);
 }
 
 function validation(cpfValue) {
-    document.getElementById('busca').setAttribute("class", "ocultar");
-    document.getElementById("resultado").removeAttribute("class", "ocultar");
-    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
+    var storage = firebase.storage();
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos) {
+
+        if (todosArquivos.items.length >= 1) {
+            next(cpfValue);
+        } else {
+            alert('CPF não cadastrado');
+        }
+    }).catch(function(error) {
+        console.log('ERRO', error);
+    });
 }
 
 function back() {
     document.getElementById("busca").removeAttribute("class", "ocultar");
     document.getElementById('resultado').setAttribute("class", "ocultar");
     document.getElementById("inputCPF").value = '';
+}
 
+function next(cpfValue) {
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById("resultado").removeAttribute("class", "ocultar");
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
 }
