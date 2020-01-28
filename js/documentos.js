4bb935377821f950f0fad8416eff42fb03faf497
Bruno Santos <brunosantosengmec@gmail.com>
2020-01-27 21:41:33 -0300

    Conectando ao Firebase

--- js/documentos.js
+++ js/documentos.js
@@ -5,5 +5,14 @@ function search() {
 }
 
 function validation(cpfValue) {
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById("resultado").removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
+}
+
+function back() {
+    document.getElementById("busca").removeAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById("inputCPF").value = '';
+
 }
---
+++ js/initFirebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDUpLbt1ukKVwRmyd5DudyZOCasI27m4to",
+    authDomain: "documentos-6439a.firebaseapp.com",
+    databaseURL: "https://documentos-6439a.firebaseio.com",
+    projectId: "documentos-6439a",
+    storageBucket: "documentos-6439a.appspot.com",
+    messagingSenderId: "976020593538",
+    appId: "1:976020593538:web:14291ff84133e3dfcacd9b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
