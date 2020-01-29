5a0e15de70bc3479146a684e27a51cd6068cd134
Bruno Santos <brunosantosengmec@gmail.com>
2020-01-29 20:56:05 -0300

    Lista de Arquivos

--- js/documentos.js
+++ js/documentos.js
@@ -1,13 +1,11 @@
 function search() {
     var cpf = document.getElementById('inputCPF').value;
-    console.log(cpf);
     validation(cpf);
 }
 
 function validation(cpfValue) {
     var storage = firebase.storage();
     storage.ref().child(cpfValue).listAll().then(function(todosArquivos) {
-
         if (todosArquivos.items.length >= 1) {
             listFiles(cpfValue);
             next(cpfValue);
@@ -27,25 +25,29 @@ function listFiles(cpfValue) {
     var linksArquivos = [];
     storage.ref().child(cpfValue).listAll().then(function(todosArquivos) {
         arquivos = todosArquivos.items;
-        console.log(arquivos);
         for (let i = 0; i < arquivos.length; i++) {
             nomeArquivos.push(arquivos[i].name);
-            storage.ref(cpfValue + "/" + nomeArquivos[i]).getDownloadURL().then(function(url) {
-                console.log(url);
-
+            storage.ref(cpfValue + '/' + nomeArquivos[i]).getDownloadURL().then(function(url) {
+                var ul = document.getElementById("list");
+                var li = document.createElement("li");
+                var listItem = '<li><a href = "' + url + '" target = "_blank">' + nomeArquivos[i] + '</a></li>';
+                li.innerHTML = listItem;
+                ul.appendChild(li);
+                linksArquivos.push(url);
+            }).catch(function(error) {
+                console.log(error);
             });
         }
     });
 }
 
-function back() {
-    document.getElementById("busca").removeAttribute("class", "ocultar");
-    document.getElementById('resultado').setAttribute("class", "ocultar");
-    document.getElementById("inputCPF").value = '';
-}
-
 function next(cpfValue) {
     document.getElementById('busca').setAttribute("class", "ocultar");
-    document.getElementById("resultado").removeAttribute("class", "ocultar");
-    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
+}
+
+function back() {
+    document.getElementById('busca').removeAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById("inputCPF").value = '';
 }
